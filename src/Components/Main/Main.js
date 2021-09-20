49cb3a233c12a18a5b7ec2f5aea59f168a911c12
Michał Sukowski <sukowskimichal@gmail.com>
2021-09-21 00:30:44 +0200

    in progress

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 import "./Main.css";
+import arrow from "./arrow.png";
 
 const Main = () => {
   return (
     <main>
       <h1>Zapisane kursy</h1>
-      <input className="search" type="text" placeholder="Jakiego kursu szukasz?" />
+      <div className="flex-container">
+        <input
+          className="search"
+          type="text"
+          placeholder="Jakiego kursu szukasz?"
+        />
+        <button className="level">
+          Poziom <img src={arrow} alt="arrow" />
+        </button>
+      </div>
     </main>
   );
 };
