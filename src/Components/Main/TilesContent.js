c21294c34e4147629ee1f831ed9703026876480d
Michał Sukowski <sukowskimichal@gmail.com>
2021-09-21 20:26:54 +0200

    mobile viewport is finished

--- src/Components/Main/TilesContent.js
+++ src/Components/Main/TilesContent.js
@@ -4,12 +4,29 @@ import Tile from "./Tile";
 import "./TilesContent.css";
 import Thumbnail1 from "./thumbnail1.png";
 import Thumbnail2 from "./thumbnail2.png";
+import Thumbnail3 from "./thumbnail3.png";
 
 const TilesContent = () => {
-  return <div className="tiles-content-container">
-      <Tile thumbnail={Thumbnail1} title="Ten kurs jest ciekawy, odpal i zrozumiesz"/>
-      {/*<Tile thumbnail ={Thumbnail2} title="Działania na liczbach i ich kolejność wykonywania" />*/}
-  </div>;
+  return (
+    <div className="tiles-content-container">
+      <Tile
+        thumbnail={Thumbnail1}
+        title="Ten kurs jest ciekawy, odpal i zrozumiesz"
+      />
+      <Tile
+        thumbnail={Thumbnail2}
+        title="Działania na liczbach i ich kolejność wykonywania"
+      />
+      <Tile
+        thumbnail={Thumbnail3}
+        title="Ten kurs jest ciekawy, odpal i zrozumiesz"
+      />
+      <Tile
+        thumbnail={Thumbnail1}
+        title="Działania na liczbach i ich kolejność wykonywania"
+      />
+    </div>
+  );
 };
 
 export default TilesContent;
